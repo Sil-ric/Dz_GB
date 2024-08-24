6c138cc6e0a0bf4ff46d0504d56cd2beae8b47a4
Sil-ric <Grinkevich2002@gmail.com>
2024-08-24 11:51:50 +0500

    дз для Gb

---
+++ 14_lesson_dz/14_lesson_dz.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+   string fresh{"Orange"};
+    cout << fresh << endl;
+    cout << fresh.length() << endl;//количество символов
+    cout << fresh[0] << endl;//Первый символ
+    cout << fresh[fresh.size() - 1] << endl;//Последный символ
+}
