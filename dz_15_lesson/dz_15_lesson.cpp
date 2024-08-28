48ed36baf4297a27f3d562a4efddd77d98d78b1c
Sil-ric <Grinkevich2002@gmail.com>
2024-08-28 21:23:38 +0500

    дз 15 урока для GB

---
+++ dz_15_lesson/dz_15_lesson.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 10;
+
+void printNumbers(int N, bool even = true) 
+{
+    if (even) 
+    {
+        cout << "even numbers: ";
+        for (int i = 0; i <= N; i += 2)
+        {
+            cout << i << " ";
+        }
+    }
+    else 
+    {
+        cout << "odd numbers: ";
+        for (int i = 1; i <= N; i += 2)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+
+
+int main()
+{
+    for (int i=0; i <= N; i+=2) 
+    {         
+        cout << i << endl;       
+    }
+    printNumbers(15, true);
+    printNumbers(20, false);
+
+}
