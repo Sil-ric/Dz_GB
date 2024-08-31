dbab61b85758d7c9addb9765e8d31de79b80f5ff
Sil-ric <Grinkevich2002@gmail.com>
2024-08-31 17:24:48 +0500

    16_Lesson_dz

---
+++ 16_Lesson_dz/16_Lesson_dz.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	const int N = 5;
+	int arr[N][N]{};
+
+	for (int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < N; j++)
+		{
+			arr[i][j] = i + j;
+			cout << arr[i][j];
+		}
+		cout << endl;
+	}
+
+	int sum = 0;
+	int day = 31;
+	int index = day % N;
+
+	for (int j = 0; j < N; j++) 
+	{
+		sum += arr[index][j];
+	}
+	cout << sum;
+}
