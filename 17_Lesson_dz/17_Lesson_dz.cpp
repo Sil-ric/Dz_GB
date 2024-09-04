853c0329ccc6bdae8433dfe36f0b18889a63c96e
Sil-ric <Grinkevich2002@gmail.com>
2024-09-04 21:03:27 +0500

    17_Lesson_Dz

---
+++ 17_Lesson_dz/17_Lesson_dz.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Vector 
+{
+private: 
+    double x = 20, y = 15, z = 10;
+public:
+    void show() 
+    {
+        cout << "X = " << x << " " << "Y = " << y << " " << "Z = " << z << endl;
+        cout << length();
+    }
+
+    double length()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+};
+
+int main()
+{
+    Vector v;
+    v.show();
+}
